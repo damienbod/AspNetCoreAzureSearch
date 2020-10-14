ac77c1a81cac13c8a5ee1af498b9850bd592cd74
damienbod <damien_bod@hotmail.com>
2020-10-14 13:56:47 +0200

    init paging search

--- Pages/Search.cshtml.cs
+++ Pages/Search.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure.Search.Documents.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,6 +14,15 @@ namespace AspNetCoreAzureSearch.Pages
         private readonly SearchProvider _searchProvider;
         private readonly ILogger<IndexModel> _logger;
 
+        public string SearchText { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageCount { get; set; }
+        public int LeftMostPage { get; set; }
+        public int PageRange { get; set; }
+        public string Paging { get; set; }
+        public int PageNo { get; set; }
+        public SearchResults<PersonCity> PersonCities;
+
         public SearchModel(SearchProvider searchProvider,
             ILogger<IndexModel> logger)
         {
@@ -24,5 +34,40 @@ namespace AspNetCoreAzureSearch.Pages
         {
 
         }
+
+
+        public async Task<ActionResult> OnPostPageAsync(SearchData model)
+        {
+            int page;
+
+            switch (model.Paging)
+            {
+                case "prev":
+                    page = PageNo - 1;
+                    break;
+
+                case "next":
+                    page = PageNo + 1;
+                    break;
+
+                default:
+                    page = int.Parse(model.Paging);
+                    break;
+            }
+
+            int leftMostPage = LeftMostPage;
+
+            model.SearchText = SearchText;
+
+            await _searchProvider.RunQueryAsync(model, page, leftMostPage).ConfigureAwait(false);
+
+            PageNo = page;
+            SearchText = model.SearchText;
+            LeftMostPage = model.LeftMostPage;
+  
+
+            return Page();
+        }
+
     }
 }
--- PersonSearch/SearchProvider.cs
+++ PersonSearch/SearchProvider.cs
@@ -40,5 +40,52 @@ namespace AspNetCoreAzureSearch
             var batch = IndexDocumentsBatch.Upload(personCities);
             await _searchClient.IndexDocumentsAsync(batch).ConfigureAwait(false);
         }
+
+        public async Task RunQueryAsync(SearchData model, int page, int leftMostPage)
+        {
+            var pageSize = 5;
+            var maxPageRange = 10;
+            var pageRangeDelta = 5;
+
+            var options = new SearchOptions
+            {
+                Skip = page * pageSize,
+                Size = pageSize,
+                IncludeTotalCount = true
+            };
+
+            // options.Select.Add("Name");
+            // options.Select.Add("CityCountry");
+
+            // For efficiency, the search call should be asynchronous, so use SearchAsync rather than Search.
+            model.PersonCities = await _searchClient.SearchAsync<PersonCity>(model.SearchText, options).ConfigureAwait(false);
+
+            // This variable communicates the total number of pages to the view.
+            model.PageCount = ((int)model.PersonCities.TotalCount + pageSize - 1) / pageSize;
+
+            // This variable communicates the page number being displayed to the view.
+            model.CurrentPage = page;
+
+            // Calculate the range of page numbers to display.
+            if (page == 0)
+            {
+                leftMostPage = 0;
+            }
+            else if (page <= leftMostPage)
+            {
+                // Trigger a switch to a lower page range.
+                leftMostPage = Math.Max(page - pageRangeDelta, 0);
+            }
+            else if (page >= leftMostPage + maxPageRange - 1)
+            {
+                // Trigger a switch to a higher page range.
+                leftMostPage = Math.Min(page - pageRangeDelta, model.PageCount - maxPageRange);
+            }
+            model.LeftMostPage = leftMostPage;
+
+            // Calculate the number of page numbers to display.
+            model.PageRange = Math.Min(model.PageCount - leftMostPage, maxPageRange);
+
+        }
     }
 }

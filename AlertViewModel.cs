3b6dba498effb24d8a7acaaef0d1a186d1d1e418
damienbod <damien_bod@hotmail.com>
2020-10-15 13:56:38 +0200

    Adding messages for the UI

---
+++ AlertViewModel.cs
@@ -0,0 +1,17 @@
+﻿
+namespace AspNetCoreAzureSearch
+{
+    public class AlertViewModel
+    {
+        public string AlertType { get; set; }
+        public string AlertTitle { get; set; }
+        public string AlertMessage { get; set; }
+
+        public AlertViewModel(string type, string title, string message)
+        {
+            AlertType = type;
+            AlertTitle = title;
+            AlertMessage = message;
+        }
+    }
+}
--- Pages/SearchAdmin.cshtml.cs
+++ Pages/SearchAdmin.cshtml.cs
@@ -13,7 +13,8 @@ namespace AspNetCoreAzureSearch.Pages
         private readonly SearchProvider _searchProvider;
         private readonly ILogger<IndexModel> _logger;
 
-        public SearchAdminModel(SearchProvider searchProvider, 
+        public AlertViewModel[] Messages = null;
+        public SearchAdminModel(SearchProvider searchProvider,
             ILogger<IndexModel> logger)
         {
             _searchProvider = searchProvider;
@@ -22,27 +23,72 @@ namespace AspNetCoreAzureSearch.Pages
 
         public void OnGet()
         {
-            
+            //Messages = new[] {
+            //    new AlertViewModel("success", "Success!", "The object was added successfully!"),
+            //    new AlertViewModel("warning", "Warning!", "The object was added with a warning!"),
+            //    new AlertViewModel("danger", "Danger!", "The object was not added!")
+            //};
         }
 
         public async Task<ActionResult> OnPostCreateIndexAsync()
         {
-            await _searchProvider.CreateIndex();
-            return Page();
+            try
+            {
+                await _searchProvider.CreateIndex();
+                Messages = new[] {
+                    new AlertViewModel("success", "Index created", "The Azure Search index was created successfully!"),
+                };
+                return Page();
+            }
+            catch (Exception ex)
+            {
+                Messages = new[] {
+                    new AlertViewModel("danger", "Error creating index", ex.Message),
+                };
+                return Page();
+            }
+
+
         }
 
         public async Task<ActionResult> OnPostAddDataAsync()
         {
-            PersonCityData.CreateTestData();
-            await _searchProvider.AddDocumentsToIndex(PersonCityData.Data);
-            return Page();
+            try
+            {
+                PersonCityData.CreateTestData();
+                await _searchProvider.AddDocumentsToIndex(PersonCityData.Data);
+                Messages = new[] {
+                    new AlertViewModel("success", "Documented added", "The Azure Search documents were uploaded!"),
+                };
+                return Page();
+            }
+            catch (Exception ex)
+            {
+                Messages = new[] {
+                    new AlertViewModel("danger", "Error adding documents", ex.Message),
+                };
+                return Page();
+            }
         }
 
         public async Task<ActionResult> OnPostDeleteIndexAsync()
         {
-            await _searchProvider.DeleteIndex();
-            return Page();
+            try
+            {
+                await _searchProvider.DeleteIndex();
+                Messages = new[] {
+                    new AlertViewModel("success", "Index Deleted!", "The Azure Search Index was successfully deleted!"),
+                };
+                return Page();
+            }
+            catch (Exception ex)
+            {
+                Messages = new[] {
+                    new AlertViewModel("danger", "Error deleting index", ex.Message),
+                };
+                return Page();
+            }
         }
-        
+
     }
 }

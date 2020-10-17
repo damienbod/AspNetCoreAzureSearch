f7db62dc7c7cf7ac86fe63469d5c1c14d46d784d
damienbod <damien_bod@hotmail.com>
2020-10-17 16:55:31 +0200

    init auto complate

---
+++ Pages/SearchAutoComplete.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetCoreAzureSearch.Pages
+{
+    public class SearchAutoCompleteModel : PageModel
+    {
+        private readonly SearchProvider _searchProvider;
+        private readonly ILogger<IndexModel> _logger;
+
+        public string SearchText { get; set; }
+
+        public SearchAutoCompleteModel(SearchProvider searchProvider,
+            ILogger<IndexModel> logger)
+        {
+            _searchProvider = searchProvider;
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+
+    }
+}

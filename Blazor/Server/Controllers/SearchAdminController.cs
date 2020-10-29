d16fb143c2d25b24c168194fba49bec4f1d9543b
damienbod <damien_bod@hotmail.com>
2020-10-29 16:08:54 +0100

    Adding query string parameters

--- Blazor/Server/Controllers/SearchAdminController.cs
+++ Blazor/Server/Controllers/SearchAdminController.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace BlazorAzureSearch.Server.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class SearchAdminController : ControllerBase
     {
         private readonly SearchProviderIndex _searchProviderIndex;
--- Blazor/Server/Controllers/SearchPagingController.cs
+++ Blazor/Server/Controllers/SearchPagingController.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace BlazorAzureSearch.Server.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class SearchPagingController : ControllerBase
     {
         private readonly SearchProviderPaging _searchProvider;

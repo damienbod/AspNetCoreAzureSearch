0af7c320b264306948a6a05a0ba62102cdb7d418
damienbod <damien_bod@hotmail.com>
2020-10-30 14:20:24 +0100

    code clean up

--- Blazor/Server/Startup.cs
+++ Blazor/Server/Startup.cs
@@ -15,8 +15,6 @@ namespace BlazorAzureSearch.Server
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<SearchProviderIndex>();
@@ -29,7 +27,6 @@ namespace BlazorAzureSearch.Server
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -40,7 +37,6 @@ namespace BlazorAzureSearch.Server
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 

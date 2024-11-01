9476e49a4805ac4f616773ef238ad289f57f91dc
damienbod <damien_bod@hotmail.com>
2024-11-01 21:39:34 +0100

    Update project start up

--- RazorPages/Program.cs
+++ RazorPages/Program.cs
@@ -1,16 +1,31 @@
-namespace AspNetCoreAzureSearch;
+using AspNetCoreAzureSearch;
 
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<SearchProviderIndex>();
+builder.Services.AddScoped<SearchProviderPaging>();
+builder.Services.AddScoped<SearchProviderAutoComplete>();
+builder.Services.AddHttpClient();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+    app.UseDeveloperExceptionPage();
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthorization();
+app.MapRazorPages();
+
+app.Run();

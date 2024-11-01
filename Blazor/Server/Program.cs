97df363fd85653548319bc598bed6a6c244b121c
damienbod <damien_bod@hotmail.com>
2024-11-01 21:43:27 +0100

    .NET 8 updates

--- Blazor/Server/Program.cs
+++ Blazor/Server/Program.cs
@@ -1,16 +1,35 @@
-﻿namespace BlazorAzureSearch.Server;
+﻿
+using BlazorAzureSearch.Server;
 
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<SearchProviderIndex>();
+builder.Services.AddScoped<SearchProviderPaging>();
+builder.Services.AddScoped<SearchProviderAutoComplete>();
+builder.Services.AddHttpClient();
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+    app.UseDeveloperExceptionPage();
+    app.UseWebAssemblyDebugging();
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
